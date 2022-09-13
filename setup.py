7451289075103719048cc5a96ad05d50651bf573
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-09-13 10:23:15 +0200

    Initial commit for package setup creation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lkpg_eunomia',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='lyadol',
+    author_email='',
+    description=''
+)
