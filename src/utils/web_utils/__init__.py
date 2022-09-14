2dd5e1efb63fe138b3bc91ad948bb903137408e5
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-09-14 11:08:39 +0200

    General web done

---
+++ src/utils/web_utils/__init__.py
@@ -0,0 +1,2 @@
+from .general_web import init_chrome_webdriver
+from .general_web import init_firefox_webdriver
--- src/utils/web_utils/general_web.py
+++ src/utils/web_utils/general_web.py
@@ -1,3 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options as firefox_options
+
+
 def init_firefox_webdriver(headless_bool: bool = None) -> webdriver:
     """		fixar inställningar för webdrivern		"""
     path = r'H:\Min enhet\selenium_browserdriver\geckodriver.exe'
@@ -6,6 +12,20 @@ def init_firefox_webdriver(headless_bool: bool = None) -> webdriver:
     if headless_bool is True:
         options.headless = True
     driver = webdriver.Firefox(executable_path=r'H:\Min enhet\selenium_browserdriver\geckodriver.exe', options=options)
+    # driver.set_window_position(2560 + 1080, 355)
+    driver.set_window_size(1000, 900)
+    driver.implicitly_wait(10)  # seconds
+    return driver
+
+
+def init_chrome_webdriver(headless_bool: bool = True, enable_file_download_redirect: bool = False) -> webdriver:
+    """		fixar inställningar för webdrivern		"""
+    options = chrome_options()
+    options.headless = headless_bool
+    if enable_file_download_redirect:
+        options.add_experimental_option("prefs", {"download.default_dirctory": r"H:\Min enhet\Python\Eunomia\downloads"})
+    srv_obj = Service(executable_path=r'H:\Min enhet\selenium_browserdriver\chromedriver103.exe')
+    driver = webdriver.Chrome(service=srv_obj, options=options)
     # driver.set_window_position(2560 + 1080, 355)
     driver.set_window_size(1000, 900)
     driver.implicitly_wait(10)  # seconds
