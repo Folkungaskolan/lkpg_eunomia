f0ad863d05cb66111b1268280681399e0a59b7fb
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-09-14 11:09:01 +0200

    reformat

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
 from .file_utils import find_student_filepath
 from .file_utils import json_wrapper
 from .file_utils import load_dict_from_json_path
+
 from .print_progress_bar import print_progress_bar
