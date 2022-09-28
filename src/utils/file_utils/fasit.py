a71cbc16caceb7d0c359871b5a10d29014b0e5fb
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-09-28 15:08:33 +0200

    projector done

--- src/utils/file_utils/fasit.py
+++ src/utils/file_utils/fasit.py
@@ -1,5 +1,4 @@
 """ funktioner för att hantera FASIT information"""
-
 import pandas as pd
 
 from settings.folders import FASIT_CSV_FILEPATH
@@ -7,7 +6,7 @@ from settings.folders import FASIT_CSV_FILEPATH
 
 def load_fasit_csv() -> pd.DataFrame:
     """ Ladda in csv filen med fasit informationen från disk"""
-    df = pd.read_csv(FASIT_CSV_FILEPATH, encoding="latin-1", sep="\t", skiprows=1)
+    df = pd.read_csv(FASIT_CSV_FILEPATH, encoding="latin-1", sep="\t", skiprows=1, dtype=str)
     # print(df.columns)
     # print(df)
     return df
@@ -31,13 +30,32 @@ def get_fasit_staff_users() -> pd.DataFrame:
                    "attribute.kundnummer",
                    "tag.användare"
                    ]]
-    # print(staff_df)
-    staff_df = staff_df[staff_df["tag.användare"] == 1]
-    print(staff_df)
+    staff_df = staff_df[staff_df["tag.användare"] == "1"]
+    staff_df.reset_index(drop=True, inplace=True)
+    staff_df.set_index("attribute.användarnamn", inplace=True)
     return staff_df
+
+
+def get_fasit_projectors() -> pd.DataFrame:
+    """ Hämta alla projektorer från fasit"""
+    df = load_fasit_csv()
+    proj_df = df[["name",
+                  "attribute.faktura",
+                  "attribute.modell",
+                  "attribute.kundnummer",
+                  "attribute.plats",
+                  "tag.videoprojektor",
+                  "attribute.tillverkare"
+                  ]]
+
+    proj_df = proj_df[proj_df["tag.videoprojektor"] == "1"]
+    proj_df.reset_index(drop=True, inplace=True)
+    proj_df.set_index("name", inplace=True)
+    print(proj_df)
+    return proj_df
 
 
 if __name__ == "__main__":
     pd.set_option('display.max_columns', 50)
     pd.set_option('display.expand_frame_repr', False)
-    get_fasit_staff_users()
+    get_fasit_projectors()
