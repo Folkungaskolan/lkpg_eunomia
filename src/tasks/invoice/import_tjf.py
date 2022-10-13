7b8d2f817aa66478be7f900c396f6e6872250c4a
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-10-13 16:13:38 +0200

    find_enhets_tjf_file funkar

--- src/tasks/invoice/import_tjf.py
+++ src/tasks/invoice/import_tjf.py
@@ -1,12 +1,20 @@
 """ Utveckling av tjänstefördelningarna """
+from pathlib import Path
+
+from settings.folders import FAKTURA_EXCEL_TJF_FOLDER
 
 
-def import_tjf():
-    """ Importerar tjänstefördelningarna från tjf filen """
+def find_enhets_tjf_file(enhet: str) -> str:
+    """ Hittar tjf filen för en enhet """
+    filelist = list(Path(FAKTURA_EXCEL_TJF_FOLDER).rglob('*.[Xx][Ll][Ss][Mm]'))
+    for filepath in filelist:
+        if enhet in str(filepath.stem):
+            return filepath
 
 
-# New in tjf
+def import_tjf_for_enhet(enhet: str) -> None:
+    """ Importerar tjänstefördelningarna för en enhet från tjf filen """
 
 
 if __name__ == '__main__':
-    pass
+    print(find_enhets_tjf_file("656"))
