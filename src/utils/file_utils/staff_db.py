07ed10344c6d817259b5d4110f102ec279d40fd6
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-10-14 12:28:25 +0200

    Refactoring staff_dbo

--- src/utils/file_utils/staff_db.py
+++ src/utils/file_utils/staff_db.py
@@ -3,10 +3,9 @@ from sqlalchemy.orm import Session
 from CustomErrors import DBUnableToCrateUser
 from db.models import Staff_dbo
 from db.mysql_db import init_db
-from utils.decorators import function_timer
 
 
-def get_staff_user_from_db(user_id: str, session: Session = None, create_on_missing: bool = False) \
+def get_staff_user_from_db_based_on_user_id(user_id: str, session: Session = None, create_on_missing: bool = False) \
         -> (Staff_dbo, Session):
     """ Get a user from the db. """
     if session is None:
@@ -16,7 +15,7 @@ def get_staff_user_from_db(user_id: str, session: Session = None, create_on_miss
 
     staff = local_session.query(Staff_dbo).filter_by(user_id=user_id).first()
     if staff is None and create_on_missing:  # if user does not exist, create it
-        session = create_staff_user(user_id=user_id, session=local_session)
+        session = create_staff_user_from_user_id(user_id=user_id, session=local_session)
         staff = local_session.query(Staff_dbo).filter_by(user_id=user_id).first()
         if staff is None:
             raise DBUnableToCrateUser("Could not create user                            2022-10-11 10:59:36")
@@ -26,7 +25,7 @@ def get_staff_user_from_db(user_id: str, session: Session = None, create_on_miss
     return staff, local_session
 
 
-def create_staff_user(user_id: str, session: Session = None) -> Session:
+def create_staff_user_from_user_id(user_id: str, session: Session = None) -> Session:
     """ Insert a user into the db. """
     if session is None:
         local_session = init_db()
@@ -85,7 +84,7 @@ def update_staff_user(user_id: str,
 
     staff = local_session.query(Staff_dbo).filter_by(user_id=user_id).first()
     if staff is None:  # if user does not exist, create it
-        session = create_staff_user(user_id=user_id, session=local_session)
+        session = create_staff_user_from_user_id(user_id=user_id, session=local_session)
         staff = local_session.query(Staff_dbo).filter_by(user_id=user_id).first()
         if staff is None:
             raise DBUnableToCrateUser("Could not create user                            2022-10-11 10:59:36")
@@ -107,27 +106,13 @@ def update_staff_user(user_id: str,
         return local_session
 
 
-@function_timer
-def calculate_missing_staff_pnr10(session: Session = None) -> Session:
-    """ Beräknar staff pnr10 """
-    if session is None:
-        local_session = session
-    else:
-        local_session = init_db()
-    staff_without_pnr10 = local_session.query(Staff_dbo).filter(Staff_dbo.pnr10 == None).all()
-    for s in staff_without_pnr10:
-        print(f"s.pnr12:{s.pnr12}  | s.pnr10:{s.pnr10}| s.pnr10 create:{s.pnr12[2:12]}")
-        s.pnr10 = s.pnr12[2:12]
-        local_session.add(s)
-    local_session.commit()
-    return local_session
-
-
 if __name__ == '__main__':
     pass
-    update_staff_user(user_id="sarqva",
-                      first_name="Sara",
-                      last_name="Qvarnström",
-                      pnr12="197307190525",
-                      email="Sara.Qvarnstrom@utb.linkoping.se",
-                      titel="Rektor")
+    calculate_missing_staff_pnr10()
+
+    # update_staff_user(user_id="sarqva",
+    #                   first_name="Sara",
+    #                   last_name="Qvarnström",
+    #                   pnr12="197307190525",
+    #                   email="Sara.Qvarnstrom@utb.linkoping.se",
+    #                   titel="Rektor")
