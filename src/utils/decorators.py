273b74c9e4a0082d4f58c3735a91d8145980812a
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-10-27 11:36:38 +0200

    save runtimes to db

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,5 +1,11 @@
 """ Funktioner för decorators """
-from time import time
+from datetime import datetime
+from time import time, sleep
+
+from database.models import RunTime_dbo
+from database.mysql_db import init_db
+
+session = init_db()
 
 
 def function_timer(func):
@@ -8,10 +14,39 @@ def function_timer(func):
     # This function shows the execution time of
     # the function object passed
     def wrap_func(*args, **kwargs):
+        """ wrapper function """
         t1 = time()
         result = func(*args, **kwargs)
         t2 = time()
-        print(f'Function {func.__name__!r}  with arg:{args} and kw:{kwargs}  executed in {(t2 - t1):.4f}s')
+        run_time = t2 - t1
+        print(f'Function {func.__name__!r}  with arg:{args} and kw:{kwargs}  executed in {(run_time):.4f}s')
+        for k in kwargs:
+            if type(kwargs[k]) == list:
+                write_runtime_to_db(func_name=func.__name__, list_length=len(kwargs[k]), run_time_sec=t2 - t1)
         return result
 
     return wrap_func
+
+
+def write_runtime_to_db(func_name: str, list_length: int, run_time_sec: float) -> None:
+    """ Skriver körtid till db """
+    print(f'Function {func_name!r}  with list length:{list_length}  executed in {(run_time_sec):.4f}s')
+    session.add(RunTime_dbo(run_date=datetime.now(),
+                            function_name=func_name,
+                            list_length=list_length,
+                            run_time_sec=run_time_sec,
+                            avg_time_sec=run_time_sec / list_length)
+                )
+    session.commit()
+
+
+@function_timer
+def demo_function_timer(l: list) -> None:
+    """ demo av function_timer """
+    for i in l:
+        sleep(0.1)
+        print(i)
+
+
+if __name__ == "__main__":
+    demo_function_timer(l=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
