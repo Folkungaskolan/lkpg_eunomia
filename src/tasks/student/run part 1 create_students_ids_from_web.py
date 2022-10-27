b11a9042a853184a81f087085410f6c5ee3f7649
Lyam Dolk <4368099+LyamDolk@users.noreply.github.com>
2022-10-27 11:37:36 +0200

    Task creation

---
+++ src/tasks/student/run part 1 create_students_ids_from_web.py
@@ -0,0 +1,5 @@
+""" kör delen med hämtning av student ids från webbsidan. """
+from utils.web_utils.student_web_scraper import _1_create_student_ids_from_web
+
+if __name__ == "__main__":
+    _1_create_student_ids_from_web()
